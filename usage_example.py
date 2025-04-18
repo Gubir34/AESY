2bd311d2b6be86bcc4961fe825123a2bf5d98dde
Gubir34 <canteoman15@gmail.com>
2025-04-18 21:44:24 +0300

    Update usage_example.py

--- usage_example.py
+++ usage_example.py
@@ -1,4 +1,12 @@
 import advanced_encryption_lib
-print(aesy_encrypt(password, message))
 
-print("Unencrypted message: " decrypted_message = aesy_decrypt('message.aesy'))
+# Get password and message from user
+password = "my_strong_password"
+message = "Secret 123 Message!"
+
+# Encryption
+advanced_encryption_lib.aesy_encrypt(password, message)
+
+# Decryption
+decrypted_message = advanced_encryption_lib.aesy_decrypt('message.aesy')
+print(f"Unencrypted message: {decrypted_message}")
