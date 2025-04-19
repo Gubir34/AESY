0547c4bd05a74e170098ea13ae15e8a8d3f494a7
Gubir34 <canteoman15@gmail.com>
2025-04-19 15:58:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'pycryptodome',
     ],
     author='Can TEOMAN',  # Yazar bilgisi
-    author_email='canteoman15@gmail.com.com',  # Yazar e-posta adresi
+    author_email='canteoman15@gmail.com',  # Yazar e-posta adresi
     description='A simple advanced encryption library for encrypting/decrypting data',
     long_description=open('README.md').read(),  # README dosyasının içeriği
     long_description_content_type='text/markdown',
@@ -17,5 +17,7 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',  # Lisans bilgisi
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Intended Audience :: Developers',
     ],
 )
